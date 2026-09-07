1a61ccea579c101bdb06e19a3f3568c64eaa7d93
FlyDecision <flydecision@flydecision.com>
2026-09-07 13:59:25 +0200

    wip. también spash

---
+++ android/app/src/main/java/com/flydecision/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.flydecision;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int DURACION_MS = 400; // igual que tu launchShowDuration actual
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+        }, DURACION_MS);
+    }
+}
